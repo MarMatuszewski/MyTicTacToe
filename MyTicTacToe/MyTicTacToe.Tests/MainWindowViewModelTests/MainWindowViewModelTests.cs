89e83ef423aebc963047a62946ba8fad48e76ff5
Marcin Matuszewski <m.j.r.matuszewski@gmail.com>
2018-06-20 00:50:55 +0100

    Added tests for can execute start game command

--- MyTicTacToe/MyTicTacToe.Tests/MainWindowViewModelTests/MainWindowViewModelTests.cs
+++ MyTicTacToe/MyTicTacToe.Tests/MainWindowViewModelTests/MainWindowViewModelTests.cs
@@ -44,5 +44,74 @@ namespace MyTicTacToe.Tests.MainWindowViewModelTests
         {
             Assert.That( _viewModel.PlayerTwo.PlayersSign, Is.EqualTo( NoughtSign ) );
         }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Disabled_When_Game_Is_In_Porgress()
+        {
+            _viewModel.CurrentGame.IsGameInProgress = true;
+
+            Assert.IsFalse( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Disabled_When_In_SinglePlayer_Mode_And_Player_Name_Is_Blank()
+        {
+            _viewModel.IsMultiplayerSelected = false;
+            _viewModel.PlayerOne.Name = string.Empty;
+
+            Assert.IsFalse( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Disabled_When_In_MultiPlayer_Mode_And_Player_One_Name_Is_Blank()
+        {
+            _viewModel.CurrentGame.IsGameInProgress = false;
+            _viewModel.IsMultiplayerSelected = true;
+            _viewModel.PlayerOne.Name = string.Empty;
+            _viewModel.PlayerTwo.Name = "PlayerTwo";
+
+            Assert.IsFalse( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Disabled_When_In_MultiPlayer_Mode_And_Player_Two_Name_Is_Blank()
+        {
+            _viewModel.CurrentGame.IsGameInProgress = false;
+            _viewModel.IsMultiplayerSelected = true;
+            _viewModel.PlayerOne.Name = "PlayerOne";
+            _viewModel.PlayerTwo.Name = string.Empty;
+
+            Assert.IsFalse( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Enabled_When_In_SinglePlayer_Mode_And_Player_One_Name_Is_Set()
+        {
+            _viewModel.CurrentGame.IsGameInProgress = false;
+            _viewModel.IsMultiplayerSelected = false;
+            _viewModel.PlayerOne.Name = "PlayerOne";
+
+            Assert.IsTrue( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void StartGame_Button_Should_Be_Enabled_When_In_MultiPlayer_Mode_And_Player_One_And_Two_Names_Are_Set()
+        {
+            _viewModel.CurrentGame.IsGameInProgress = false;
+            _viewModel.IsMultiplayerSelected = true;
+            _viewModel.PlayerOne.Name = "PlayerOne";
+            _viewModel.PlayerTwo.Name = "PlayerTwo";
+
+            Assert.IsTrue( _viewModel.StartGameCommand.CanExecute( null ) );
+        }
+
+        [Test]
+        public void PlayerTwo_Name_Should_Be_Set_To_Computer_When_Game_Is_Started_In_MultiPlayer_Mode()
+        {
+            _viewModel.IsMultiplayerSelected = false;
+            _viewModel.StartGameCommand.Execute( null );
+
+            Assert.That( _viewModel.PlayerTwo.Name, Is.EqualTo( "Computer" ) );
+        }
     }
 }
